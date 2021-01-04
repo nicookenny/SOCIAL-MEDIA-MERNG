1bd5ede2aa80ea3c526a82c476953707115d0265
Nicolas <nicokenny98@gmail.com>
2021-01-04 10:29:47 -0300

    asd

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -16,7 +16,7 @@ const authLink = setContext(() => {
 	};
 });
 const httpLink = createHttpLink({
-	uri: 'http://localhost:5000',
+	uri: 'https://pure-cove-63652.herokuapp.com/',
 });
 
 const client = new ApolloClient({
